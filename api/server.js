1c325722c252d0ea8a323b8f2eba05aac5e6236f
Raul Ferent <44873170+Rallfy@users.noreply.github.com>
2024-09-12 00:40:02 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ethers = require("ethers");
 const { Connection, PublicKey } = require("@solana/web3.js");
 require("dotenv").config();
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
